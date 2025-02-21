548b1890929a9058f49fb433001f59244c94bff9
Carlos Herrera <91082864+endercarteam@users.noreply.github.com>
2025-02-21 13:24:27 -0500

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -1,68 +1,29 @@
-#!/bin/bash
+version: '3.8'
 
-echo "=== Benchmark de Multiplicación de Matrices ==="
+services:
+  benchmark:
+    build: .
+    volumes:
+      - /var/run/docker.sock:/var/run/docker.sock
+      - .:/benchmark
+    privileged: true
 
-# Función para dibujar línea de la tabla
-draw_line() {
-    printf "+---------------+------------------+\n"
-}
+  c:
+    build: ./c
+    image: matrix-c
 
-# Función para ejecutar benchmark y extraer tiempo
-run_benchmark() {
-    local lang=$1
-    echo "Ejecutando benchmark para $lang..."
-    
-    local output=$(docker run --rm "matrix-$lang" 2>&1)
-    local time
-    
-    case $lang in
-        "c")
-            time=$(echo "$output" | grep "Tiempo ejecución en C++" | awk '{print $5}')
-            ;;
-        "go")
-            time=$(echo "$output" | grep "En milisegundos:" | awk '{print $3}')
-            ;;
-        "java")
-            time=$(echo "$output" | grep "Tiempo de ejecución en Java:" | awk '{print $6}')
-            ;;
-        "javascript")
-            time=$(echo "$output" | grep "Tiempo de ejecución en JavaScript:" | awk '{print $5}')
-            ;;
-        "python")
-            time=$(echo "$output" | grep "Python:" | awk '{print $6}')
-            ;;
-    esac
-    
-    echo "$time"
-}
+  go:
+    build: ./go
+    image: matrix-go
 
-# Construir imágenes
-echo "Construyendo imágenes..."
-for lang in c go java javascript python; do
-    docker build -t "matrix-$lang" "./$lang"
-done
+  java:
+    build: ./java
+    image: matrix-java
 
-# Array asociativo para almacenar resultados
-declare -A results
+  javascript:
+    build: ./javascript
+    image: matrix-javascript
 
-# Ejecutar benchmarks
-for lang in c go java javascript python; do
-    results[$lang]=$(run_benchmark $lang)
-done
-
-# Mostrar resultados en tabla
-echo -e "\n=== Resultados del Benchmark ===\n"
-draw_line
-printf "| %-13s | %-16s |\n" "Lenguaje" "Tiempo (ms)"
-draw_line
-
-# Ordenar y mostrar resultados
-for lang in $(for k in "${!results[@]}"; do echo "$k ${results[$k]}"; done | sort -k2n); do
-    name=${lang% *}
-    time=${results[$name]}
-    if [[ ! -z "$time" ]]; then
-        printf "| %-13s | %16.2f |\n" "$name" "$time"
-    fi
-done
-
-draw_line
+  python:
+    build: ./python
+    image: matrix-python
